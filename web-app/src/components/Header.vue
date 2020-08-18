9635b34b8ba76cb8a3b2075c1a3ccaeae18c1299
Gabriel <gabrielalexmv@gmail.com>
2020-08-18 17:18:35 -0400

    Header component added

---
+++ web-app/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-navbar variant="dark" type="dark">
+      <b-navbar-brand>
+        <b-icon icon="card-list"></b-icon> Permisos
+      </b-navbar-brand>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
